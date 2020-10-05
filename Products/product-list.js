7edd9be5c92e823a0e13a8f0d6b87e76c220e257
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-10-05 14:24:56 -0700

    html, css, js, & product properties established

---
+++ Products/product-list.js
@@ -0,0 +1,12 @@
+// ### Step 1: Design List
+
+// Work out the static design for one product in your list. This will help you to flush out what data will be needed. Make sure to include an "Add" button whose value is the product `id`.
+
+// Property | Description
+// ---|---
+// `id` | a unique string that identifies this product
+// `name` | user friendly name of the product
+// `image` | image file name for this image (should live in `assets`)
+// `description` | a longer description of the product
+// `category` | the category this product belongs to, limit to one
+// `price` | the price the user will pay
---
+++ data.js
@@ -0,0 +1,11 @@
+const boardGames = [
+    {
+        id: 'arkham',
+        title: 'Arkham Horror 3rd Edition',
+        publisher: 'Chaosium',
+        cover: 'arkham.png',
+        price: 64.99,
+        description: 'Arkham Horror: The Card Game is a cooperative Living Card Game® set amid a backdrop of Lovecraftian horror. As the Ancient Ones seek entry to our world, one to two investigators (or up to four with two Core Sets) work to unravel arcane mysteries and conspiracies.        Their efforts determine not only the course of your game, but carry forward throughout whole campaigns, challenging them to overcome their personal demons even as Arkham Horror: The Card Game blurs the distinction between the card game and roleplaying experiences.'
+        expansion: false,        
+    }
+]
