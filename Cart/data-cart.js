872f80eaece3a5de65972e944009a968557e0319
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-10-06 14:31:40 -0700

    loaded cart

---
+++ Cart/data-cart.js
@@ -0,0 +1,13 @@
+export const cart = [{
+    id: 'arkham',
+    price: 64.99
+}, {
+    id: 'betrayal',
+    price:29.99
+}, {
+    id: 'boss',
+    price: 21.99
+}, {
+    id: 'gloomhaven',
+    price: 99.99
+}];
